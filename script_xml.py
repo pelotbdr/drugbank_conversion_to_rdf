e700c350393a71a55687b1d229c2cbf0747c6667
pelotbdr <pelot.bdr@hotmail.fr>
2019-02-20 11:39:00 +0100

    Design finished, now to the writing

--- script_xml.py
+++ script_xml.py
@@ -23,24 +23,42 @@ datab = xml.dom.minidom.parse(file)
 for drug in datab.getElementsByTagName('drug'):
 	if 'type' in drug.attributes :
 		name = getText(drug.getElementsByTagName('name')[0].childNodes)
-		print(name)
+		print(name)																# Drug Name
 		id= getText(drug.getElementsByTagName('drugbank-id')[0].childNodes)
-		print(id)
+		print(id)																# Drug Drugbank ID
 		atcCode=drug.getElementsByTagName('atc-code')[0].getAttribute('code')
-		print(atcCode)
+		print(atcCode)															# Drug ATC code
 		listInterID = []
 		listInterDesc = []
 		for drugInter in drug.getElementsByTagName('drug-interactions') :
-				for inter in drugInter.getElementsByTagName('drug-interaction') :
-					listInterID.append(getText(inter.getElementsByTagName('drugbank-id')[0].childNodes))
-					listInterDesc.append(getText(inter.getElementsByTagName('description')[0].childNodes))
+				for drugInter2 in drugInter.getElementsByTagName('drug-interaction') :
+					listInterID.append(getText(drugInter2.getElementsByTagName('drugbank-id')[0].childNodes))						# ID of Drugs interacting with our starting Drug
+					listInterDesc.append(getText(drugInter2.getElementsByTagName('description')[0].childNodes))						# Description of the interactions
 		externalList = {}
 		for exter_ref in drug.getElementsByTagName('external-identifiers') :
 			for exter_ref2 in exter_ref.getElementsByTagName('external-identifier') :
-				externalList[getText(exter_ref2.getElementsByTagName('resource')[0].childNodes)] = getText(exter_ref2.getElementsByTagName('identifier')[0].childNodes)
-		print(externalList)
-					
+				externalList[getText(exter_ref2.getElementsByTagName('resource')[0].childNodes)] = getText(exter_ref2.getElementsByTagName('identifier')[0].childNodes)		# External IDs of the starting Drug
+		enzymes = []
+		for pathway in drug.getElementsByTagName('pathways') :
+			for pathway2 in pathway.getElementsByTagName('pathway') :
+				for enz in pathway2.getElementsByTagName('enzymes') :
+					for i in range(len(enz.getElementsByTagName('uniprot-id'))) :
+						enzymes.append(getText(enz.getElementsByTagName('uniprot-id')[i].childNodes))					# Enzymes implicated in the pathway of the starting Drug
+		drugTarget = {}
+		externalListTarget = {}
+		goClassif = {}
+		for target in drug.getElementsByTagName('targets') :
+			for target2 in drug.getElementsByTagName('target') :
+				drugTarget[getText(target2.getElementsByTagName('id')[0].childNodes)] = getText(target2.getElementsByTagName('actions')[0].getElementsByTagName('action')[0].childNodes)  # Drug Target
+				for polypep in target2.getElementsByTagName('polypeptide') :
+					for exter_ref_target in polypep.getElementsByTagName('external-identifiers') :
+						for exter_ref_target2 in exter_ref_target.getElementsByTagName('external-identifier') :
+							externalListTarget[getText(exter_ref_target2.getElementsByTagName('resource')[0].childNodes)] = getText(exter_ref_target2.getElementsByTagName('identifier')[0].childNodes) # External IDs of the target
+					for go in polypep.getElementsByTagName('go-classifiers') :
+						for go2 in go.getElementsByTagName('go-classifier') :
+							if getText(go2.getElementsByTagName('category')[0].childNodes) in goClassif :																		# GO classifiers of the target
+								goClassif[getText(go2.getElementsByTagName('category')[0].childNodes)] += "," + getText(go2.getElementsByTagName('description')[0].childNodes)
+							else :
+								goClassif[getText(go2.getElementsByTagName('category')[0].childNodes)] = getText(go2.getElementsByTagName('description')[0].childNodes)
 		
-		
-		
-
+			
